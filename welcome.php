337b6a20831f686991b2e56783c3502718fb7431
charity553 <57391531+charity553@users.noreply.github.com>
2023-04-18 11:53:12 -0500

    basic PHP GET and POST requests

---
+++ welcome.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<!-- Sample get form to dislay submiited data using POST-->
+
+Welcome <?php
+ echo $_POST["name"]; 
+?><br>
+Your email address is: 
+<?php
+ echo $_POST["email"]; 
+ ?>
+
+</body>
+</html>
---
+++ welcome_get.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<!-- Sample get form to dislay submiited data using GET -->
+Welcome 
+<?php
+ echo $_GET["name"]; 
+?><br>
+Your email address is:
+ <?php 
+ echo $_GET["email"]; 
+ ?>
+
+</body>
+</html>
